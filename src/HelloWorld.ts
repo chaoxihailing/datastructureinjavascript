715c83ab0fb8bca5096925f0efc6b7e6b2aa0790
Bob <Bob.Ye@agileact.com>
2017-12-29 14:31:14 +0800

    example

---
+++ src/HelloWorld.ts
@@ -0,0 +1,18 @@
+import * as _ from "lodash";
+let hello = "hello";
+let world = "nodejs";
+let helloworld = hello + " " + world;
+console.log(helloworld);
+
+// var avariable = "Outer";
+
+// function afun() {
+//     var avariable = "middle";
+//     return _.map([3], function (e) {
+//         var avariable = "in";
+
+//         return [avariable, e].join(' ');
+//     });
+// }
+
+// console.log(afun());
